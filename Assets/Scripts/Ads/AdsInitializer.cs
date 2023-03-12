1ae1b942b895b909f15dd4386b137ec7caccc05c
KishanPoddar <kishanpoddar2002@gmail.com>
2023-03-12 22:52:06 +0530

    Implemented Unity Ads

---
+++ Assets/Scripts/Ads/AdsInitializer.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsInitializer : MonoBehaviour, IUnityAdsInitializationListener
+{
+    [SerializeField] string _androidGameId;
+    [SerializeField] string _iOSGameId;
+    [SerializeField] bool _testMode = true;
+    private string _gameId;
+
+    [Header("Elements")]
+    [SerializeField] private InterstitialAd interstitialAd;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        InitializeAds();
+    }
+
+    public void InitializeAds()
+    {
+        _gameId = (Application.platform == RuntimePlatform.IPhonePlayer)
+            ? _iOSGameId
+            : _androidGameId;
+        Advertisement.Initialize(_gameId, _testMode, this);
+    }
+
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Unity Ads initialization complete.");
+        interstitialAd.LoadAd();
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.Log($"Unity Ads Initialization Failed: {error.ToString()} - {message}");
+        InitializeAds();
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -59,6 +59,7 @@ public class GameManager : MonoBehaviour
 
     public void ReloadLevel()
     {
+        InterstitialAd.instance.ShowAd();
         SceneManager.LoadScene("Game");
     }
 
